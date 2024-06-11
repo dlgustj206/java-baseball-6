8cc917255d1b8961044d46f07d3cd14adf83e059
dlgustj206 <dlgustj206@naver.com>
2024-06-11 17:03:20 +0900

    Fix: "ClientNumber에서 중복 숫자 검증 오류 수정"
    hasNoDuplicateNumber 메서드를 수정하여 입력 숫자에서 중복된 숫자를 올바르게 감지하고 처리하도록 수정

--- src/main/java/baseball/model/ClientNumber.java
+++ src/main/java/baseball/model/ClientNumber.java
@@ -5,25 +5,30 @@ import java.util.Set;
 
 public class ClientNumber {
 
-    private String clientNumber;
+    private final String clientNumber;
+
+    public ClientNumber(String number) {
+        isStringLengthCorrect(number);
+        isValidDigitClientNumber(number);
+        hasNoDuplicateNumber(number);
+        this.clientNumber = number;
+    }
+
+    public ClientNumber() {
+        this.clientNumber = "123";
+    }
 
     public String getClientNumber() {
         return clientNumber;
     }
 
-    public void validateClientNumber(String clientNumber) {
-        isStringLengthCorrect(clientNumber);
-        isValidDigitClientNumber(clientNumber);
-        hasNoDuplicateNumber(clientNumber);
-    }
-
-    public static void isStringLengthCorrect(String number) {
+    public void isStringLengthCorrect(String number) {
         if(number.length() != 3) {
             throw new IllegalArgumentException("ERROR: 입력값은 3자리여야 합니다.");
         }
     }
 
-    public static void isValidDigitClientNumber(String number) {
+    public void isValidDigitClientNumber(String number) {
         for(int i = 0; i < number.length(); i++) {
             char c = number.charAt(i);
             if(c < '1' || c > '9') {
@@ -32,12 +37,13 @@ public class ClientNumber {
         }
     }
 
-    public static void hasNoDuplicateNumber(String number) {
+    public void hasNoDuplicateNumber(String number) {
         Set<Character> set = new HashSet<>();
-        for(char c : number.toCharArray()) {
-            if(set.contains(c)) {
+        for (char c : number.toCharArray()) {
+            if (set.contains(c)) {
                 throw new IllegalArgumentException("ERROR: 입력값에 중복된 숫자가 없어야 합니다.");
             }
+            set.add(c);
         }
     }
 }
