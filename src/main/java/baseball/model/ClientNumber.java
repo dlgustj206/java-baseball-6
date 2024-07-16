7cd202bed5523b662c9d9ddf8771bd59aa57554e
dlgustj206 <dlgustj206@naver.com>
2024-07-16 17:33:25 +0900

    Refactor: "ClientNumber 클래스 예외 및 유효성 검증 로직 리팩토링"
    - validateClientNumber 메서드를 수정하여 `Validator.validateUserNumber`를 사용하도록 변경하고, 발생한 `ValidationError`을 적절히 `IllegalArgumentException`으로 처리하도록 수정함.

--- src/main/java/baseball/model/ClientNumber.java
+++ src/main/java/baseball/model/ClientNumber.java
@@ -1,30 +1,17 @@
 package baseball.model;
 
-import java.util.HashSet;
-import java.util.Set;
+import baseball.utils.ValidationError;
+import baseball.utils.Validator;
 
 public class ClientNumber {
 
     public ClientNumber() {}
 
     public void validateClientNumber(String number) {
-        if(number.length() != 3) {
-            throw new IllegalArgumentException("ERROR: 3자리가 아니요.");
-        }
-
-        for(int i = 0; i < number.length(); i++) {
-            char c = number.charAt(i);
-            if(c < '1' || c > '9') {
-                throw new IllegalArgumentException("ERROR: 입력값은 1에서 9 사이의 숫자여야 합니다.");
-            }
-        }
-
-        Set<Character> set = new HashSet<>();
-        for (char c : number.toCharArray()) {
-            if (set.contains(c)) {
-                throw new IllegalArgumentException("ERROR: 입력값에 중복된 숫자가 없어야 합니다.");
-            }
-            set.add(c);
+        try {
+            Validator.validateUserNumber(number);
+        } catch (ValidationError e) {
+            throw new IllegalArgumentException(e.getMessage());
         }
     }
 }
